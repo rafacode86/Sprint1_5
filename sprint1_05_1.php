2bcb7cc123f5bcb54daa54b932be63add01debfb
Rafa Bello <rafabp986@outlook.com>
2025-05-22 13:04:00 +0200

    Ejercicio 1 añadido

---
+++ sprint1_05_1.php
@@ -0,0 +1,30 @@
+<?php
+    /*Necessitem crear un tipus de dades que representi un animal. 
+    Els animals tenen un nom, ara bé, no és el mateix el so de la “parla” 
+    d’un gos, que el d’un gat. Per tant, necessitem crear altres tipus de 
+    dades que ens ajudin a programar aquests comportaments. Bàsicament, 
+    volem un mètode makeSound() que mostri un missatge diferent si es tracta 
+    d’un gos (per exemple,“Bup, bup!”) o un gat (per exemple “Meu!”).*/
+
+    class Animal {
+        public function makeSound() {
+            echo "Specify type of animal";
+        }
+    }
+
+    class Dog extends Animal {
+        public function makeSound() {
+            echo "Bup, Bup! \n";
+        }
+    }
+    class Cat extends Animal {
+        public function makeSound() {
+            echo "Meu! \n";
+        }
+    }
+
+    $dog = new Dog();
+    $dog->makeSound();
+    $cat = new Cat();
+    $cat->makeSound();
+?>
